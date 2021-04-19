2a7308f9b693b0b825a612e0891d73e2f96c03ef
dodo0211 <freean2468@gmail.com>
2021-04-19 09:16:22 +0900

    daum 사이트 레이아웃 따기

---
+++ 20210415/js/test.js
@@ -0,0 +1,14 @@
+var arr = []; 
+arr["하나"] = 1;
+arr["참"] = true;
+arr["자바스크립트"] = "JavaScript";
+
+document.write(arr["참"] + "<br>");
+document.write(arr.length + "<br>");
+document.write(arr[0] + "<br>");
+
+let arr = {
+    n_val: 1,
+    b_val: true,
+    s_val: "JavaScript"
+};
